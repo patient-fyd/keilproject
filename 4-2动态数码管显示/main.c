cd25e33a5d7de47dd97800f1e66ffdaf5433cab3
patient-fyd <patient.fyd@gmail.com>
2023-01-20 19:38:23 +0800

    动态数码管显示

---
+++ 4-2动态数码管显示/main.c
@@ -0,0 +1,59 @@
+#include <REGX52.H>
+
+unsigned char NixieTable[]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
+
+void Delay1ms(unsigned int xms)		//@12.000MHz
+{
+	unsigned char i, j;
+	while(xms)
+	{
+		i = 2;
+		j = 239;
+		do
+		{
+			while (--j);
+		} while (--i);
+		xms--;
+	}
+}
+
+
+void Nixie(unsigned char Location,Number)
+{
+	switch(Location)
+	{
+		case 1:
+			P2_4=1;P2_3=1;P2_2=1;break;
+		case 2:
+			P2_4=1;P2_3=1;P2_2=0;break;
+		case 3:
+			P2_4=1;P2_3=0;P2_2=1;break;
+		case 4:
+			P2_4=1;P2_3=0;P2_2=0;break;
+		case 5:
+			P2_4=0;P2_3=1;P2_2=1;break;
+		case 6:
+			P2_4=0;P2_3=1;P2_2=0;break;
+		case 7:
+			P2_4=0;P2_3=0;P2_2=1;break;
+		case 8:
+			P2_4=0;P2_3=0;P2_2=0;break;		
+	}
+	P0=NixieTable[Number];
+	Delay1ms(1);
+	P0=0x00;
+}
+void main()
+{
+	
+	while(1)
+	{
+		Nixie(6,5);
+//		Delay1ms(20);
+		Nixie(7,2);
+//		Delay1ms(20);
+		Nixie(8,0);
+//		Delay1ms(20);
+	}
+	
+}
