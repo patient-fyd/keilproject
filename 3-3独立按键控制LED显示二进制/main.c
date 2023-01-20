04ccfcc5843a314ba30ac674be56ae8c24340b21
patient-fyd <patient.fyd@gmail.com>
2023-01-20 19:37:01 +0800

    独立按键控制LED显示二进制

---
+++ 3-3独立按键控制LED显示二进制/main.c
@@ -0,0 +1,35 @@
+#include <REGX52.H>
+
+void Delay1ms(unsigned int x)		//@12.000MHz
+{
+	unsigned char i, j;
+	while(x)
+	{
+		i = 2;
+		j = 239;
+		do
+		{
+			while (--j);
+		} while (--i);
+		
+		x--;
+	}
+}
+
+void main()
+{
+
+	 while(1)
+	 {
+		 if(P3_1==0)
+		 {
+			 Delay1ms(20);
+			 while(P3_1==0);
+			 Delay1ms(20);
+
+			 P2--;
+			 
+
+		 }
+	 }
+}
